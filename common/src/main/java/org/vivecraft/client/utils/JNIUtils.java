de3fb16fa7d64327716666285d3f86b11e73f16b
fayer3 <bayer.florian@aon.at>
2026-02-27 23:16:48 +0100

    lwjgl 3.4 test

---
+++ common/src/main/java/org/vivecraft/client/utils/JNIUtils.java
@@ -0,0 +1,132 @@
+package org.vivecraft.client.utils;
+
+import org.lwjgl.PointerBuffer;
+import org.lwjgl.system.MemoryStack;
+import org.lwjgl.system.MemoryUtil;
+import org.lwjgl.system.libffi.FFICIF;
+import org.lwjgl.system.libffi.FFIType;
+import org.lwjgl.system.libffi.LibFFI;
+import org.vivecraft.client_vr.settings.VRSettings;
+
+import java.nio.ByteBuffer;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class JNIUtils {
+
+    private static final Map<String, FFIInfo> FFIs = new HashMap<>();
+
+    private static FFIInfo get(String format) {
+        FFIInfo info = FFIs.get(format);
+        VRSettings.LOGGER.warn("calling: {}", Thread.currentThread().getStackTrace()[3]);
+        if (info == null) {
+            String[] parameters = format.split("_");
+            FFICIF cif = FFICIF.malloc();
+            PointerBuffer argTypes = null;
+            if (!parameters[0].isEmpty()) {
+                argTypes = PointerBuffer.allocateDirect(parameters[0].length());
+                for (int i = 0; i < parameters[0].length(); i++) {
+                    argTypes.put(i, getType(parameters[0].charAt(i)));
+                }
+            }
+            int ret = LibFFI.ffi_prep_cif(cif, LibFFI.FFI_DEFAULT_ABI, getType(parameters[1].charAt(0)),
+                argTypes);
+            if (ret != LibFFI.FFI_OK) {
+                throw new RuntimeException("FFI error: " + ret);
+            }
+            info = new FFIInfo(cif, parameters[0].toCharArray());
+            FFIs.put(format, info);
+        }
+        return info;
+    }
+
+    public static float callF(String signature, long __functionAddress, Object... args) {
+        try (MemoryStack stack = MemoryStack.stackPush()) {
+            FFIInfo info = get(signature);
+
+            PointerBuffer pointers = getPointers(stack, info.args, args);
+
+            ByteBuffer returnValue = stack.malloc(4);
+
+            LibFFI.ffi_call(info.cif, __functionAddress, returnValue, pointers);
+
+            return returnValue.getFloat(0);
+        }
+    }
+
+    public static int callI(String signature, long __functionAddress, Object... args) {
+        try (MemoryStack stack = MemoryStack.stackPush()) {
+            FFIInfo info = get(signature);
+
+            PointerBuffer pointers = getPointers(stack, info.args, args);
+
+            ByteBuffer returnValue = stack.malloc(4);
+
+            LibFFI.ffi_call(info.cif, __functionAddress, returnValue, pointers);
+
+            return returnValue.getInt(0);
+        }
+    }
+
+    public static void callV(String signature, long __functionAddress, Object... args) {
+        try (MemoryStack stack = MemoryStack.stackPush()) {
+            FFIInfo info = get(signature);
+
+            PointerBuffer pointers = getPointers(stack, info.args, args);
+
+            LibFFI.ffi_call(info.cif, __functionAddress, null, pointers);
+        }
+    }
+
+    public static boolean callZ(String signature, long __functionAddress, Object... args) {
+        try (MemoryStack stack = MemoryStack.stackPush()) {
+            FFIInfo info = get(signature);
+
+            PointerBuffer pointers = getPointers(stack, info.args, args);
+
+            ByteBuffer returnValue = stack.malloc(1);
+
+            LibFFI.ffi_call(info.cif, __functionAddress, returnValue, pointers);
+
+            return returnValue.get(0) == 1;
+        }
+    }
+
+    private static PointerBuffer getPointers(MemoryStack stack, char[] types, Object[] args) {
+        if (types.length > 0 && args == null || (args != null && types.length != args.length)) {
+            throw new IllegalArgumentException("arguments needed but not enough supplied! types:" + Arrays.toString(types) + ", args:" + (args == null ? "null" : Arrays.toString(Arrays.stream(args).map(o -> o.getClass().getSimpleName()).toArray())));
+        }
+        if (args == null) {
+            return null;
+        }
+        PointerBuffer pointers = stack.mallocPointer(args.length);
+        for (int i = 0; i < args.length; i++) {
+            switch (types[i]) {
+                case 'I', 'U' -> pointers.put(i, MemoryUtil.memAddress(stack.ints((int) args[i])));
+                case 'J' -> pointers.put(i, MemoryUtil.memAddress(stack.longs((long) args[i])));
+                case 'F' -> pointers.put(i, MemoryUtil.memAddress(stack.floats((int) args[i])));
+                case 'S' -> pointers.put(i, MemoryUtil.memAddress(stack.shorts((short) args[i])));
+                case 'Z' ->
+                    pointers.put(i, MemoryUtil.memAddress(stack.bytes((boolean) args[i] ? (byte) 1 : (byte) 0)));
+                case 'P' -> pointers.put(i, stack.pointers((long) args[i]).address());
+            }
+        }
+        return pointers;
+    }
+
+    private static FFIType getType(char c) {
+        return switch (c) {
+            case 'I' -> LibFFI.ffi_type_sint32;
+            case 'U' -> LibFFI.ffi_type_uint32;
+            case 'J' -> LibFFI.ffi_type_uint64;
+            case 'P' -> LibFFI.ffi_type_pointer;
+            case 'F' -> LibFFI.ffi_type_float;
+            case 'Z' -> LibFFI.ffi_type_uint8;
+            case 'S' -> LibFFI.ffi_type_uint16;
+            default -> throw new IllegalArgumentException("unknown parameter type: " + c);
+        };
+    }
+
+    private record FFIInfo(FFICIF cif, char[] args) {}
+}
